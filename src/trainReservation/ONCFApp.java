79037890c142f61ffe11b0c603c46905b7d59969
MOBR4 <135473717+MOBR4@users.noreply.github.com>
2024-02-16 18:36:38 +0100

    first page of welcome
    par mohamed Berkat

--- src/trainReservation/ONCFApp.java
+++ src/trainReservation/ONCFApp.java
@@ -1,16 +1,18 @@
 package trainReservation;
 
 
-public class Pricipale {
+public class ONCFApp {
 
 
 	public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-            	
+
                 new WelcomeIHM().setVisible(true);
             }
         });
     }
-    
+
 }
+
+
